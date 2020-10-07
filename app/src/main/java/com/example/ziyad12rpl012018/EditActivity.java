948cb3ab001f7e3cdb76319401b29201b02ce317
Ziyadarzaq <ziyadxrpl2018smkrus@gmail.com>
2020-10-07 15:15:09 +0700

    fist

---
+++ app/src/main/java/com/example/ziyad12rpl012018/EditActivity.java
@@ -0,0 +1,16 @@
+package com.example.ziyad12rpl012018.Model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ziyad12rpl012018.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
